c939b5d4cc71bde79a459ba81d177e1bdcc4d87f
wuhuiluo <510426343@qq.com>
2020-12-20 14:06:55 +0800

    latest commit

--- top.js
+++ top.js
@@ -399,3 +399,226 @@
 //     b: undefined - 2,
 //     c: undefined - 3
 // }
+// function test() {
+//     return a
+//     a = 1
+//     function a() {}
+//     var a = 2
+// }
+// console.log(test());
+
+// AO = {
+//     a: undefined - function a() {}
+// }
+// function test(){
+//     console.log(b); 
+//     if(a){
+//       var b = 2;
+//     }
+//     c = 3;
+//     // console.log(c); 
+//   }
+//   var a;
+//   test();
+//   a = 1;
+//   console.log(a);
+//   console.log(c);
+
+// function test(a, b) {
+
+// }
+// console.log(test.name);
+// console.log(test.length);
+
+// function test1() {
+//     function test2() {
+//         var b = 2
+//         console.log(a);
+//     }
+//     var a = 1
+//     return test2()
+// }
+// var c = 3
+// var test3 = test1()
+// test3()
+
+// let teacher = {
+//     name: 'whl',
+//     age: 32,
+//     sex: 'male',
+//     height: 176,
+//     weight: 130,
+//     teach: function () {
+//         console.log('I am teaching JS');
+//     },
+//     smoke: function () {
+//         console.log('I am smoking');
+//     },
+//     eat: function () {
+//         console.log('I am having a dinner');
+//     }
+// }
+
+// 查找
+// console.log(teacher.name);
+// teacher.teach()
+// teacher.smoke()
+// teacher.eat()
+
+// 增加
+
+// teacher.address = '北京'
+// teacher.drink = function () {
+//     console.log('I am drinking beer');
+// }
+// console.log(teacher.address);
+// teacher.drink()
+
+// 修改
+
+// teacher.teach = function () {
+//     console.log('I am teaching vue');
+// }
+// teacher.teach()
+// 删除(只有对象才能删除)
+// delete teacher.address;
+// delete teacher.teach
+// console.log(teacher);
+// 对象是通过构造函数实例化出来的
+// 构造函数基础 对象是通过构造函数实例化创建出来的
+// var obj = new Object()
+// obj.name = '张三'
+// obj.sex = '男士'
+// console.log(obj);
+// function Teacher() {
+//     this.name = '张三'
+//     this.sex = '男士'
+//     this.smoke = function () {
+//         console.log('I am Smoking')
+//     }
+// }
+
+// var teacher1 = new Teacher()
+// var teacher2 = new Teacher()
+// teacher1.name = 'wuhuiluo'
+// console.log(teacher1.name);
+// console.log(teacher2.name);
+// GO = {
+//     Teacher: function () { ... }
+// }
+// function Teacher(opt) {
+//     this.name = opt.name
+//     this.sex = opt.sex
+//     this.weight = opt.weight
+//     this.course = opt.course
+//     this.smoke = function () {
+//         this.weight--
+//         console.log(this.weight);
+//     }
+//     this.eat = function () {
+//         this.weight++
+//         console.log(this.weight)
+//     }
+// }
+
+// var t1 = new Teacher({
+//     name: '张三',
+//     sex: '男士',
+//     weight: 130,
+//     course: 'JS'
+// });
+// var t2 = new Teacher({
+//     name: '李四',
+//     sex: '女士',
+//     weight: 90,
+//     course: 'Vue'
+// });
+// console.log(t1);
+// console.log(t2);
+// new Number new String new Boolean
+// var a = 'abc'
+// console.log(a)
+// // 对象都是new 出来的
+// var aa = new String('abc')
+// aa.name = 'aa'
+// console.log(aa);
+// // var bb = aa + 'bcd'
+// // console.log(bb);
+
+// var arr = [1, 2, 3, 4, 5]
+// arr.length = 3
+// console.log(arr);
+// console.log('wuhuiluo' + 10);
+// var name = 'wuhuiluo'
+// name += 10
+// var type = typeof (name)
+// if (type.length === 6) {
+//     type.text = 'string'
+// }
+// console.log(type.text);
+// let a = 3
+// a.leg = a
+// console.log(a.leg);
+// let b = '123'
+// console.log(b.length);
+// let b = '123'
+// console.log(new String())
+
+// var name = 'wuhuiluo'
+// name += 10
+// var type = new String(typeof (name))
+// if (type.length === 6) {
+//     type.text = 'string'
+// }
+// console.log(type)
+// console.log(type.text)
+
+// function test(a, b, c) {
+//     var d = 1
+//     this.a = a
+//     this.b = b
+//     this.c = c
+
+//     function f() {
+//         d++;
+//         console.log(d)
+//     }
+//     this.g = f
+// }
+// 通过构造函数创建出来的对象
+// var test1 = new test()
+// test1.g()
+// test1.g()
+// var test2 = new test()
+// test2.g() // 2
+
+// AO = {
+
+// }
+
+// GO = {
+
+// }
+
+// function b(x, y, a) {
+//     a = 10
+//     console.log(arguments[2])
+// }
+
+// b(1, 2, 3)
+
+// function Test() {
+
+// }
+// 原型prototype function对象的一个属性
+// console.log(Test.prototype) // 对象
+// 构造函数
+function Foo(name, age) {
+    this.name = name
+    this.age = age
+}
+
+Foo.prototype.sex = '男士'
+var foo = new Foo('wuhuiuo', 21)
+console.log(foo.name);
+console.log(foo.sex);
