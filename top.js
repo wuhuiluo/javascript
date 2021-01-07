33856b5a3d3fb204991c0365eedab54ef945ede9
wuhuiluo <510426343@qq.com>
2021-01-07 14:25:58 +0800

    latest commit

--- top.js
+++ top.js
@@ -1255,24 +1255,49 @@ const obj4 = new new Function()
 // stu1.sing()
 // stu2.sing()
 // console.log(stu1.sing === stu2.sing);
-function Star(name) {
-    this.name = name;
+// function Star(name) {
+//     this.name = name;
 
-    //   (1) 首先看obj对象身上是否有dance方法， 如果有， 则执行对象身上的方法
-    this.dance = function () {
-        console.log(this.name + '1');
-    }
-}
+//     //   (1) 首先看obj对象身上是否有dance方法， 如果有， 则执行对象身上的方法
+//     this.dance = function () {
+//         console.log(this.name + '1');
+//     }
+// }
 
-//(2) 如果没有dance方法， 就去构造函数原型对象prototype身上去查找dance这个方法。
-Star.prototype.dance = function () {
-    console.log(this.name + '2');
-};
+// //(2) 如果没有dance方法， 就去构造函数原型对象prototype身上去查找dance这个方法。
+// Star.prototype.dance = function () {
+//     console.log(this.name + '2');
+// };
 
-//(3) 如果再没有dance方法， 就去Object原型对象prototype身上去查找dance这个方法。
-Object.prototype.dance = function () {
-    console.log(this.name + '3');
-};
-//(4) 如果再没有， 则会报错。
-let obj = new Star('小红');
-obj.dance();
+// //(3) 如果再没有dance方法， 就去Object原型对象prototype身上去查找dance这个方法。
+// Object.prototype.dance = function () {
+//     console.log(this.name + '3');
+// };
+// //(4) 如果再没有， 则会报错。
+// let obj = new Star('小红');
+// obj.dance();
+// Promise.resolve().then(() => {
+//     console.log(1); // resolve
+//     throw new Error('123')
+// }).catch(() => {
+//     console.log(2);
+// }).then(() => {
+//     console.log(3);
+// })
+// async function fn1() {
+//     return 100
+// }
+// const res1 = fn1()
+// res1.then(data => {
+//     console.log(data);
+// })
+!(async function() {
+    const p1 = Promise.resolve(300)
+    const data = await p1
+    console.log(data);
+})()
+
+!(async function(){
+    const p1 = await 400 // await Promise.resolve(400)
+    console.log(p1);
+})()
